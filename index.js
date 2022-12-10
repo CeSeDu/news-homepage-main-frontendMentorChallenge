cefa49961aceb6802ff12e3c2e5298b480a70395
CeSeDu <senad8127@gmail.com>
2022-12-10 19:27:04 +0300

    js commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+hamburger = document.querySelector(".hamburger");
+hamburger.onclick = function(){
+    navbarSm = document.querySelector(".navbarSm");
+    navbarSm.classList.toggle("active");
+}
+hamburgerExit = document.querySelector(".hamburgerExit");
+hamburgerExit.onclick =function(){
+    navbarSm = document.querySelector(".navbarSm");
+    navbarSm.classList.toggle("active")
+}
